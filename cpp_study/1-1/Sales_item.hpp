66475e2744d6bae7b8f9254fd680f2ae1f448764
goodname427 <1335438725@qq.com>
2023-11-13 22:39:57 +0800

    saving

---
+++ cpp_study/1-1/Sales_item.hpp
@@ -0,0 +1,16 @@
+class Sales_item
+{
+private:
+    /* data */
+public:
+    Sales_item(/* args */);
+    ~Sales_item();
+};
+
+Sales_item::Sales_item(/* args */)
+{
+}
+
+Sales_item::~Sales_item()
+{
+}
---
+++ cpp_study/1-1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Sales_item.hpp"
+
+int main()
+{
+    Sales_item book;
+
+    // std::cin >> book;
+
+    // std::cout << book << std::endl;
+    
+    return 0;
+}
