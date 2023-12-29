9106c3f0108af33f16e5be4adacc61b3da6af957
goodname427 <1335438725@qq.com>
2023-12-29 17:56:30 +0800

    saving

---
+++ cpp_study/c++ 11/variadic_template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print()
+{
+    cout << endl;
+}
+
+void print(int i)
+{
+    print();
+}
+
+template <typename T, typename... Types>
+void print(const int &i, const T &firstArg, const Types &...args)
+{
+    cout << firstArg << " ";
+    print(i, args...);
+}
+
+template <typename... Types>
+void print(const Types &...args)
+{
+    // print(args...);
+    cout << "what's wrong with you? bro?" << endl;
+}
+
+int main()
+{
+    print("ASD", 2, 3, "asdasd", 's', 213.2);
+    print(2, 3, "asdasd", 's', 213.2);
+    return 0;
+}
