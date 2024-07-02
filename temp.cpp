18b28f827b6d9356de18852795dd97c8b3123617
goodname427 <1335438725@qq.com>
2024-07-02 21:05:03 +0800

    saving

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define PI 3.1415f
+
 int main()
 {
+    float a = 600, b = 2048;
+    for (float i = 0; i < PI; i += 0.1)
+    {
+        cout << a * cos(i) + b << ",";
+    }
+
     return 0;
 }
