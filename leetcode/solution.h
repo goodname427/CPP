48de63458cbff31ef08c8b687325212f51ed1984
goodname427 <1335438725@qq.com>
2024-07-13 14:10:34 +0800

    saving

--- leetcode/solution.h
+++ leetcode/solution.h
@@ -3,53 +3,55 @@ using namespace std;
 
 
 class Solution {
-private:
-    unordered_map<int, int> cached;
-    int max_no;
-
-    inline int get_no(int n, int i, int j)
+public:
+    template<class Accessor>
+    int bs(Accessor accessor, int target, int l, int r)
     {
-        return ((n - i - 1) * n) + ((n - i - 1) % 2 ? (n - j) : j + 1);
-    }
-
-    inline void get_pos(int n, int no, int &i, int &j)
-    {
-        i = n - 1 - ((no - 1) / n);
-        no =  ((no - 1) % n);
-        j = (n - i - 1) % 2 ? n - no - 1 : no; 
-    }
-
-    inline int process(const vector<vector<int>> &board, int no)
-    {
-        if (no == max_no)
-            return 0;
-        
-        if (no > max_no)
-            return INT_MAX;
-
-        cout << no << endl;
-
-        if (cached.count(no))
-            return cached[no];
-
-        cached[no] = INT_MAX - 1;
-        int i, j, res = INT_MAX - 1;
-        for (int k = 1; k <= 6; k++)
+        if (l > r)
         {
-            if (no + k > max_no)
-                break;
-
-            get_pos(board.size(), no + k, i, j);
-            res = min(res, min(process(board, board[i][j] != -1 ? board[i][j] : no + k), INT_MAX - 1) + 1);
+            return -1;
         }
 
-        return cached[no] = res;
+        int mid = l + ((r - l) >> 1);
+        int left;
+        
+        if (target == accessor(mid))
+        {
+            return mid;
+        }
+
+        if (target < accessor(mid))
+        {
+            return bs(accessor, target, l, mid - 1);
+        }
+        else
+        {
+            int right = bs(accessor, target, mid + 1, r);
+            return right == -1 ? mid : right;
+        }
     }
 
-public:
-    int snakesAndLadders(const vector<vector<int>>& board) 
+    bool searchMatrix(const vector<vector<int>>& matrix, int target) 
     {
-        max_no = board.size() * board.size();
-        return process(board, 1);
+        if (matrix.size() == 0)
+        {
+            return false;
+        }
+
+        int row = bs([&matrix](int i){ return matrix[i][0]; }, target, 0, matrix.size());
+
+        if (row == -1)
+        {
+            return false;
+        }
+
+        if (matrix[row][0] == target)
+        {
+            return true;
+        }
+
+        int col = bs([&matrix, row](int i){ return matrix[row][i]; }, target, 0, matrix[row].size());
+
+        return (col != -1 && matrix[row][col] == target);
     }
 };
